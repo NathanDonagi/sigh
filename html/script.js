70c710fa2d6bbee874a343997a649a50fb50d89b
TheBannana32 <>
2022-12-03 04:28:01 +0000

    Initial commit

---
+++ html/script.js
@@ -0,0 +1,38 @@
+var socket;
+var usernameInput
+var chatIDInput;
+var messageInput;
+var chatRoom;
+var messages = [];
+var delay = true;
+
+function onload(){
+  socket = io();
+  usernameInput = document.getElementById("NameInput");
+  messageInput = document.getElementById("ComposedMessage");
+
+  socket.on("recieve", function(message){
+    if (messages.length < 9){
+      messages.push(message);
+    }
+    else{
+      messages.shift();
+      messages.push(message);
+    }
+    for (i = 0; i < messages.length; i++){
+        document.getElementById("Message"+i).innerHTML = messages[i];
+        document.getElementById("Message"+i).style.color = "#303030";
+    }
+  })
+}
+
+function Connect(){
+  socket.emit("set_name", usernameInput.value);
+}
+
+function Send(){
+  if ( messageInput.value.replace(/\s/g, "") != ""){
+    socket.emit("send", messageInput.value);
+    messageInput.value = "";
+  }
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const path = require("path");
+
+const app = express();
+const httpserver = http.Server(app);
+const io = socketio(httpserver);
+
+const gamedirectory = path.join(__dirname, "html");
+
+app.use(express.static(gamedirectory));
+
+httpserver.listen(3000);
+
+var usernames = [];
+
+io.on('connection', function(socket){
+  var clientIp = socket.request.connection.remoteAddress;
+  console.log(clientIp)
+  
+  socket.on("set_name", function(username){
+    if (username != ""){
+      usernames[socket.id] = username;
+     io.emit("recieve", username + " has entered the chat.");
+    }
+  })
+
+  socket.on("send", function(message){
+    io.emit("recieve", usernames[socket.id] +" : " + message);
+  })
+})
